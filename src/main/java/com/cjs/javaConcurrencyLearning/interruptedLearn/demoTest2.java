6698fc29ad4bd42471575d45fc203370e7cdd8e1
LiquidFox <ginyz@foxmail.com>
2020-09-20 12:57:08 +0800

    单线程模式的改进

---
+++ src/main/java/com/cjs/javaConcurrencyLearning/interruptedLearn/demoTest2.java
@@ -0,0 +1,28 @@
+package com.cjs.javaConcurrencyLearning.interruptedLearn;
+
+import java.util.concurrent.TimeUnit;
+
+public class demoTest2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    TimeUnit.MINUTES.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        thread.start();
+        thread.interrupt();
+
+        while (true){
+            TimeUnit.MILLISECONDS.sleep(100);
+            System.out.println(thread.getState());
+            System.out.println(thread.isInterrupted());
+        }
+    }
+}
---
+++ src/main/java/com/cjs/javaConcurrencyLearning/threadConnect/singleExample.java
@@ -0,0 +1,79 @@
+/*
+* 执行会报错
+* */
+
+package com.cjs.javaConcurrencyLearning.threadConnect;
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+
+public class singleExample {
+    public static void main(String[] args) {
+        EventQueue eventQueue = new EventQueue();
+
+
+        new Thread(() -> {
+            for (int i = 0; ; i++) {
+                try {
+                    eventQueue.setLinkedList();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"thread1").start();
+
+        new Thread(() -> {
+            for (int i = 0; ; i++) {
+                try {
+                    eventQueue.setLinkedList();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"thread2").start();
+
+        new Thread(() -> {
+            for (int i = 0; ; i++) {
+                try {
+                    eventQueue.getLinkedList();
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
+    }
+}
+
+class EventQueue {
+    private final LinkedList<String> linkedList = new LinkedList<>();
+    private final int max = 10;
+
+    public void setLinkedList() throws InterruptedException {
+        synchronized (linkedList) {
+            if (linkedList.size() >= max) {  //ｉｆ改成ｗｈｉｌｅ可解决
+                linkedList.wait();
+            }
+
+            System.out.println(Thread.currentThread().getName()+",push:" + linkedList.size());
+            linkedList.add(String.valueOf(linkedList.size()));
+            linkedList.notify(); //多线程使用ｎｏｔｉｆｙＡｌｌ
+
+        }
+    }
+
+    public void getLinkedList() throws InterruptedException {
+        synchronized (linkedList) {
+            if (linkedList.isEmpty()) { //ｉｆ改成ｗｈｉｌｅ可解决
+                linkedList.wait();
+            }
+
+            System.out.println("pop,name:" + linkedList.poll());
+            linkedList.notify();  //多线程使用ｎｏｔｉｆｙＡｌｌ
+        }
+    }
+}
